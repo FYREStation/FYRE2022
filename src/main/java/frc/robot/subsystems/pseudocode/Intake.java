bbb9a29156d58d4fc292e7c2f3d2fb57ac8911e9
Ian <66448861+IanGaunt@users.noreply.github.com>
2022-01-23 12:28:00 -0800

    Create Intake.java

---
+++ src/main/java/frc/robot/subsystems/pseudocode/Intake.java
@@ -0,0 +1,65 @@
+/*
+   [ Intake Pseudocode ]
+   
+   Used to express how the intake code
+   would work in a fictional world where
+   everything I write works first time.
+   
+   last edited by Ian G. 1/23/22
+   
+*/
+
+// Grab package for frc.robot
+package frc.robot.SubsystemPsuedocode;
+
+// Imports a series of basic APIs
+import edu.wpi.first.wpilibj.PWMVictorSPX; // Random motor I found, may not be used in final product
+import edu.wpi.first.wpilibj2.command.SubsystemBase; // Subsystem framework 
+
+// import edu.wpi.first.wpilibj.Timer; // Timer for incremental wheel turning, possibly used?
+
+// Copy class Subsystem base to Intake
+public class Intake extends SubsystemBase {
+
+    static PWMVictorSPX motor;
+
+    // A series of booleans that defines if the round / intake should be started. 
+    static boolean intakeOn = false;
+    static boolean roundStarted = true;
+
+    static void roundStart(boolean isIntakeDown){
+
+        /*
+            Since the actual intake system will be down
+            when the round is started, we cannot run the
+            wheels the moment the round begins. So I used
+            a placeholder. 
+        */
+
+        if (isIntakeDown){
+            intakeOn = true;
+        }
+    }
+    
+    // Runs a motor all the time. Not sure how fast the while loop will run
+    static void intake(){
+        while (intakeOn == true){
+            motor.set(0.2);
+        }
+    }
+
+    /* 
+        Here is where the color tracking stuff would go.
+        Since we don't have a drive function presently set
+        up, I'm omitting it from this pseudocode, due to 
+        my unfamiliarity with the WpiLIB API. 
+
+        I also do not know anything about GRIP, so I don't 
+        know how the vision processing would actually work.
+    */
+
+    public static void main(String[] args) {
+        Intake.roundStart(true);
+        Intake.intake();
+    }
+}
