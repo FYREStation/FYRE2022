2b9b49c9bf23f410eee5523c045e9e4f161bb57f
Meh27 <98196539+Meh27@users.noreply.github.com>
2022-02-03 19:43:33 -0800

    Drivetrain.java cleaned

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -25,56 +25,56 @@ public class DriveTrain extends SubsystemBase {
 
     /* leftDriveMotors + rightDriveMotors are empty variables in Constants. Once we can start
     messing with actual motors, PLEASE update them / this code!! */
-    // private final VictorSPX left_motorA = new VictorSPX(Constants.leftDriveMotorA);
-    // private final VictorSPX left_motorB = new VictorSPX(Constants.leftDriveMotorB);
+    private final Spark left_motorA = new Spark(Constants.leftDriveMotorA);
+    private final Spark left_motorB = new Spark(Constants.leftDriveMotorB);
 
-    // private final VictorSPX right_motorA = new VictorSPX(Constants.rightDriveMotorA);
-    // private final VictorSPX right_motorB = new VictorSPX(Constants.rightDriveMotorB);
+    private final Spark right_motorA = new Spark(Constants.rightDriveMotorA);
+    private final Spark right_motorB = new Spark(Constants.rightDriveMotorB);
 
-    // private final MotorControllerGroup left_motors = new MotorControllerGroup(left_motorA, left_motorB);
-    // private final MotorControllerGroup right_motors = new MotorControllerGroup(right_motorA, right_motorB);
+    private final MotorControllerGroup left_motors = new MotorControllerGroup(left_motorA, left_motorB);
+    private final MotorControllerGroup right_motors = new MotorControllerGroup(right_motorA, right_motorB);
 
-    // private final DifferentialDrive differential_drive = new DifferentialDrive(left_motors, right_motors);
+    private final DifferentialDrive differential_drive = new DifferentialDrive(left_motors, right_motors);
 
     // /* Set a series of variables. For more information on what they do, check out this link: 
     // https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/wpilibj/MotorSafety.html#setExpiration(double) */
 
-    // private static int expiration_dur = 99999; // Change this variable to affect expiration time. 
-    // private static boolean safety_toggle = false; // Change this variable to toggle safety mode. 
+    private static int expiration_dur = 99999; // Change this variable to affect expiration time. 
+    private static boolean safety_toggle = true; // Change this variable to toggle safety mode. 
     
     public DriveTrain (){
-        // /* Not fully sure if we need these. Kept them anyway.
+         // Not fully sure if we need these. Kept them anyway.
         
-        //     left_motor.setExpiration(expiration_dur);
-        //     right_motor.setExpiration(expiration_dur);
+             left_motor.setExpiration(expiration_dur);
+             right_motor.setExpiration(expiration_dur);
 
-        //     left_motor.setSafetyEnabled(safety_toggle);
-        //     right_motor.setSafetyEnabled(safety_toggle);
-        // */
+             left_motor.setSafetyEnabled(safety_toggle);
+             right_motor.setSafetyEnabled(safety_toggle);
+         
 
-        // differential_drive.setExpiration(expiration_dur);
-        // differential_drive.setSafetyEnabled(safety_toggle);
+         differential_drive.setExpiration(expiration_dur);
+         differential_drive.setSafetyEnabled(safety_toggle);
     }
 
     /* Once you guys figure out your encoders, please add them here! :) I am going to 
     need them for the arcadeDrive subsystem. */
 
-    // // Two drive functions for arcade drive method and tank drive method. 
-    // public void arcadeDrive (double move_speed, double rot_speed){
-    //     if (Constants.invertedDrive) {
-    //         differential_drive.arcadeDrive(move_speed, rot_speed);
-    //     } else {
-    //         differential_drive.arcadeDrive(-move_speed, rot_speed);
-    //     }
-    // }
+    // Two drive functions for arcade drive method and tank drive method. 
+     public void arcadeDrive (double move_speed, double rot_speed){
+         if (Constants.invertedDrive) {
+             differential_drive.arcadeDrive(move_speed, rot_speed);
+         } else {
+             differential_drive.arcadeDrive(-move_speed, rot_speed);
+         }
+     }
 
-    // public void tankDrive (double move_speed_l, double move_speed_r) {
-    //     if (Constants.invertedDrive) {            
-    //         differential_drive.tankDrive(move_speed_l, move_speed_r);
-    //     } else {
-    //         differential_drive.tankDrive(-move_speed_l, -move_speed_r);
-    //     }    
-    // }
+     public void tankDrive (double move_speed_l, double move_speed_r) {
+         if (Constants.invertedDrive) {            
+             differential_drive.tankDrive(move_speed_l, move_speed_r);
+         } else {
+             differential_drive.tankDrive(-move_speed_l, -move_speed_r);
+         }    
+     }
 
     // Periodic commands. 
     @Override
