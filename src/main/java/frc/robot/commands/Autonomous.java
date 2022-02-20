f5b9d3e295adbc27fb2517830800128ac5453cd6
Ian <iangaunt999@gmail.com>
2022-02-19 17:29:36 -0800

    Started to flesh out Autonomous distance calculation

--- src/main/java/frc/robot/commands/Autonomous.java
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -27,11 +27,13 @@ public class Autonomous extends CommandBase {
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        auto_distance(1, true);
+    }
 
     @Override   
     public void execute() { 
-        fake_auto();  
+       
     }
 
     @Override
@@ -43,15 +45,35 @@ public class Autonomous extends CommandBase {
     public void end(boolean interrupted) {}
 
     // Short movement command. Not fully functional. 
-    public void fake_auto() { 
-        int i = 0; 
-        while (i < 2000) { 
-            drive_train.tankDrive(-0.7, 0.7);
-            i += 1; 
-        }
-        i = 0; 
+    public void auto_distance(double distance, boolean forward) { 
+        double position = drive_train.get_encoder_distance("right");
+        double destination;
 
-        System.out.println("ENDED!"); 
-        isFinished();
+        double speed = 30; 
+        double speed_pwm = ((speed * 0.465) + 44.1) / 100;
+        
+        if (speed_pwm > 0.8 || speed_pwm < 0.5) {
+            System.out.println("Speed does not fall in the threshold. Current speed " + speed_pwm + ".");
+        } else {
+            if (forward) { 
+                destination = position + distance;
+
+                while (position < destination) { 
+                    System.out.println("Current position " + position);
+                    position = drive_train.get_encoder_distance("right");
+
+                    drive_train.tankDrive(speed_pwm + 0.03, speed_pwm);
+                }
+            } else { 
+                destination = position - distance;
+
+                while (position > destination) { 
+                    System.out.println("Current position " + position);
+                    position = drive_train.get_encoder_distance("right");
+
+                    drive_train.tankDrive(-(speed_pwm + 0.03), -speed_pwm);
+                }
+            }
+        }
     }
 }
