ab328fba3fdbce0bcc1f275f2fcf4e3692514e03
Ian <iangaunt999@gmail.com>
2022-03-05 10:25:10 -0800

    Removal of normal intake run to prevent motor from smoking

--- src/main/java/frc/robot/commands/IntoIntake.java
+++ src/main/java/frc/robot/commands/IntoIntake.java
@@ -39,7 +39,8 @@ public class IntoIntake extends CommandBase {
 				m_intake.run_intake_backward();
 				break;
 			default:
-				m_intake.run_intake_normal();
+				m_intake.stopIntake(); 
+				//m_intake.run_intake_normal();
 				break;
 		}
   	}
